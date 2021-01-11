b8d316ac7c25f33543bc208fb7f10eaa8533e5dd
Jose Victor Pacajoj Mejia <jpacajoj.mp@gmail.com>
2021-01-10 18:08:32 -0600

    Unitary test in node server

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const app = express()
 app.use(cors())
 
 app.use(logger('dev'))
+
 // Lectura y parseo del body
 app.use(express.json())
 
--- server/index.js
+++ server/index.js
@@ -1,35 +1,14 @@
 /**
  * Dependencias del módulo.
  */
-const app = require('./config')
+const app = require('./app')
 const http = require('http')
-
-/**
- * Obtenga el puerto del entorno y guárdelo en Express.
- */
-
-const port = normalizePort('4000')
-app.set('port', port)
-
-/**
- * Crear un servidor HTTP.
- */
-
-const server = http.createServer(app)
-
-/**
- * Escuche en el puerto provisto, en todas las interfaces de red.
- */
-
-server.listen(port)
-server.on('error', onError)
-server.on('listening', onListening)
 
 /**
  * Normalizar el puerto en number, string, or false.
  */
 
-function normalizePort (val) {
+const normalizePort = (val) => {
   const port = parseInt(val, 10)
 
   if (isNaN(port)) {
@@ -47,7 +26,7 @@ function normalizePort (val) {
  * Detector de eventos para el evento "error" del servidor HTTP.
  */
 
-function onError (error) {
+const onError = (error) => {
   if (error.syscall !== 'listen') {
     throw error
   }
@@ -73,10 +52,33 @@ function onError (error) {
  * Event listener for HTTP server "listening" event.
  */
 
-function onListening () {
+const onListening = () => {
   const addr = server.address()
   const bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port
-  console.log('Servidor corriendo en puerto ' + bind)
+  console.log('Servidor corriendo en ' + bind)
 }
+
+/**
+ * Obtenga el puerto del entorno y guárdelo en Express.
+ */
+
+const port = normalizePort(process.env.PORT || 80)
+app.set('port', port)
+
+/**
+ * Crear un servidor HTTP.
+ */
+
+const server = http.createServer(app)
+
+/**
+ * Escuche en el puerto provisto, en todas las interfaces de red.
+ */
+
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
+
+module.exports = server
